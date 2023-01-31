bec81dcb5d69573983a58b2a69aa5c22001b0083
Mateusz <mat.bereda@gmail.com>
2023-01-31 21:06:30 +0100

    init

---
+++ src/main/java/pl/edu/wszib/jdbc/User.java
@@ -0,0 +1,66 @@
+package pl.edu.wszib.jdbc;
+
+public class User {
+    private int id;
+    private String login;
+    private String password;
+    private Role role;
+
+    public User(int id, String login, String password, Role role) {
+        this.id = id;
+        this.login = login;
+        this.password = password;
+        this.role = role;
+    }
+
+    public User() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("User{");
+        sb.append("id=").append(id);
+        sb.append(", login='").append(login).append('\'');
+        sb.append(", password='").append(password).append('\'');
+        sb.append(", role=").append(role);
+        sb.append('}');
+        return sb.toString();
+    }
+
+    public enum Role {
+        USER,
+        ADMIN
+    }
+}
